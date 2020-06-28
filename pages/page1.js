de7aa2d698181ad735813b401c7f4327e67dfce2
ekalii <andoune0719@gmail.com>
2020-06-28 12:51:55 +0900

    using static file, HTML head tag, style tag

--- pages/page1.js
+++ pages/page1.js
@@ -1,7 +1,23 @@
+import Head from 'next/head';
+import Icon from '../static/icon.jpg';
+
 function Page1() {
   return(
     <div>
       <p>This is home page</p>
+      <img src={Icon} />
+      <Head>
+        <title>page1</title>
+      </Head>
+      <Head>
+        <meta name='description' content='hello world'></meta>
+      </Head>
+      <style jsx>{`
+        p { 
+          color: blue;
+          font-size: 18pt;
+        }
+      `}</style>
     </div>
   );
 }
